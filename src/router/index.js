a6c8e4e50e763a031c60833fe35d8e0f2ed1de1c
Howard Miller <howardsmiller@gmail.com>
2026-02-15 20:34:23 +0000

    changing to web history

--- src/router/index.js
+++ src/router/index.js
@@ -44,7 +44,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   scrollBehavior(to, from, savedPosition) {
       // always scroll to top
       return { top: 0 }
