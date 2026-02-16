cf37c5895b8f4fbbfbac85b7aaccaf215c33ece3
Howard Miller <howardsmiller@gmail.com>
2026-02-16 15:21:29 +0000

    adding matomo stuff

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/index.js'
+import VueMatomo from 'vue-matomo'
 
 const app = createApp(App);
 app.use(router);
-app.mount('#app')
+app.use(VueMatomo, {
+            // Configure your matomo server and site by providing
+            host: 'http://matomo.srps.org.uk/',
+            siteId: 1,
+        });
+app.mount('#app');
+
+window._paq.push(['trackPageView']); //To track pageview
