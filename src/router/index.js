4564ccb9c8065bfa7ac783a6ac237ebc04265791
Howard Miller <howardsmiller@gmail.com>
2026-03-17 10:55:28 +0000

    adding link to birkhill signal box

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,10 @@ const routes = [
     component: () => import('@/views/HomePage.vue')
   },
   {
+    path: '/birkhill',
+    redirect: '/page/birkhill',
+  },
+  {
     path: '/timetable',
     name: 'TimetablePage',
     component: () => import('@/views/TimetablePage.vue')
